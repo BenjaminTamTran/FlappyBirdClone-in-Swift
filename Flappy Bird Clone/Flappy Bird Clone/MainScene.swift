4e482b6955eb7551eacb01d6cd625245648e1479
BenjaminTamTran <benjamintamtran@gmail.com>
2015-11-05 14:48:09 +0700

    Skyline + Ground + Spawn pipe

--- Flappy Bird Clone/Flappy Bird Clone/MainScene.swift
+++ Flappy Bird Clone/Flappy Bird Clone/MainScene.swift
@@ -16,16 +16,19 @@ class MainScene : SKScene, SKPhysicsContactDelegate {
     let worldCategory: UInt32 = 1 << 1
     let pipeCategory: UInt32 = 1 << 2
     let scoreCategory: UInt32 = 1 << 3
-    
+    let kVerticalPipeGap: CGFloat = 100.0
     
     // Game property
     var bird = SKSpriteNode()
     var skyColor = SKColor()
-    var pipeTexture1 = SKTexture()
+    var pipeTextTure1 = SKTexture()
     var pipeTextTure2 = SKTexture()
     var moving = SKNode()
     var pipes = SKNode()
     var canRestart = false
+    var resetSound = SKAction()
+    var swapSound = SKAction()
+    var movePipesAndRemove = SKAction()
     
     override init(size: CGSize) {
         super.init(size: size)
@@ -57,6 +60,7 @@ class MainScene : SKScene, SKPhysicsContactDelegate {
         bird.physicsBody?.allowsRotation = false
         bird.physicsBody?.categoryBitMask = birdCategory
         bird.physicsBody?.collisionBitMask = worldCategory | pipeCategory
+        bird.physicsBody?.contactTestBitMask = worldCategory | pipeCategory
         self .addChild(bird)
         
         // create main node
@@ -67,8 +71,8 @@ class MainScene : SKScene, SKPhysicsContactDelegate {
         let groundTexture = SKTexture(imageNamed: "Ground")
         groundTexture.filteringMode = SKTextureFilteringMode.Nearest
         
-        let moveGroundSprite = SKAction .moveByX(-groundTexture.size().width*2, y: 0, duration: 0.02 * Double(groundTexture.size().width) * 2.0)
-        let resetGroundSprite = SKAction .moveByX(groundTexture.size().width*2, y: 0, duration: 0.0)
+        let moveGroundSprite = SKAction .moveByX(-groundTexture.size().width * 2, y: 0, duration: 0.02 * Double(groundTexture.size().width) * 2.0)
+        let resetGroundSprite = SKAction .moveByX(groundTexture.size().width * 2, y: 0, duration: 0.0)
         let moveGroundSpritesForever = SKAction .repeatActionForever(SKAction.sequence([moveGroundSprite, resetGroundSprite]))
         
         for (var i = 0; i < 2 + Int(self.frame.size.width / (groundTexture.size().width * 2)) ; i++) {
@@ -78,6 +82,89 @@ class MainScene : SKScene, SKPhysicsContactDelegate {
             sprite .runAction(moveGroundSpritesForever)
             moving .addChild(sprite)
         }
+        
+        // Skyeline
+        let skylineTexture = SKTexture(imageNamed: "Skyline")
+        skylineTexture.filteringMode = SKTextureFilteringMode.Nearest
+        
+        let moveSkyelineSprite = SKAction .moveByX(-skylineTexture.size().width * 2, y: 0, duration: 0.1 * Double(skylineTexture.size().width * 2))
+        let resetSkyelineSprite = SKAction.moveByX(skylineTexture.size().width * 2, y: 0, duration: 0)
+        let moveSkylelineSpriteForever = SKAction .repeatActionForever(SKAction.sequence([moveSkyelineSprite, resetSkyelineSprite]))
+    
+        for (var i = 0; i < 2 + Int(self.frame.size.width / (skylineTexture.size().width * 2)) ; i++) {
+            let sprite = SKSpriteNode(texture: skylineTexture)
+            sprite.setScale(2.0)
+            sprite.zPosition = -10
+            sprite.position = CGPointMake(CGFloat(i) * sprite.size.width, sprite.size.height / 2 + groundTexture.size().height * 2)
+            sprite .runAction(moveSkylelineSpriteForever)
+            moving .addChild(sprite)
+        }
+        
+        // Create ground physics container
+        let dummyGround = SKNode()
+        dummyGround.position = CGPointMake(0, groundTexture.size().height)
+        dummyGround.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(self.frame.size.width, groundTexture.size().height * 2))
+        dummyGround.physicsBody?.dynamic = false
+        dummyGround.physicsBody?.categoryBitMask = worldCategory
+        self .addChild(dummyGround)
+        
+        // Create pipes
+        pipeTextTure1 = SKTexture(imageNamed: "Pipe1")
+        pipeTextTure1.filteringMode = SKTextureFilteringMode.Nearest
+        pipeTextTure2 = SKTexture(imageNamed: "Pipe2")
+        pipeTextTure2.filteringMode = SKTextureFilteringMode.Nearest
+        
+        let distanceToMove = self.frame.size.width + 2 * pipeTextTure1.size().width
+        let movePipes = SKAction .moveByX(-distanceToMove, y: 0, duration: 0.01 * Double(distanceToMove))
+        let removePipes = SKAction .removeFromParent()
+        movePipesAndRemove = SKAction.sequence([movePipes, removePipes])
+        
+        // action Spwan Pipes with selector
+        let spawn = SKAction .performSelector(Selector("spawnPipes"), onTarget: self)
+        let delay = SKAction .waitForDuration(2.0)
+        let spwanThenDelay = SKAction.sequence([spawn, delay])
+        let spawnThenDelayForever = SKAction .repeatActionForever(spwanThenDelay)
+        self .runAction(spawnThenDelayForever)
+        
+        resetSound = SKAction .playSoundFileNamed("Chomp.wav", waitForCompletion: false)
+        swapSound = SKAction .playSoundFileNamed("Ka-Ching", waitForCompletion: false)
+    }
+    
+    func spawnPipes() {
+        let pipePair = SKNode()
+        pipePair.position = CGPointMake(self.frame.size.width + pipeTextTure1.size().width, 0)
+        pipePair.zPosition = -5
+        
+        let y = CGFloat(arc4random_uniform(UInt32(self.frame.size.height / 3)))
+        
+        let pipe1 = SKSpriteNode(texture: pipeTextTure1)
+        pipe1 .setScale(2.0)
+        pipe1.position = CGPointMake(0, y)
+        pipe1.physicsBody = SKPhysicsBody(rectangleOfSize: pipe1.size)
+        pipe1.physicsBody?.dynamic = false
+        pipe1.physicsBody?.categoryBitMask = pipeCategory
+        pipe1.physicsBody?.contactTestBitMask = birdCategory
+        pipePair.addChild(pipe1)
+        
+        let pipe2 = SKSpriteNode(texture: pipeTextTure2)
+        pipe2 .setScale(2.0)
+        pipe2.position = CGPointMake(0, y + pipe1.size.height + kVerticalPipeGap)
+        pipe2.physicsBody = SKPhysicsBody(rectangleOfSize: pipe2.size)
+        pipe2.physicsBody?.dynamic = false
+        pipe2.physicsBody?.categoryBitMask = pipeCategory
+        pipe2.physicsBody?.contactTestBitMask = birdCategory
+        pipePair.addChild(pipe2)
+        
+        let contactNode = SKNode()
+        contactNode.position = CGPointMake(pipe1.size.width + bird.size.width / 2, CGRectGetMidY(self.frame))
+        contactNode.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(pipe2.size.width, self.frame.size.height))
+        contactNode.physicsBody?.dynamic = false
+        contactNode.physicsBody?.categoryBitMask = scoreCategory
+        contactNode.physicsBody?.contactTestBitMask = birdCategory
+        pipePair.addChild(contactNode)
+        
+        pipePair .runAction(movePipesAndRemove)
+        pipes .addChild(pipePair)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -87,7 +174,8 @@ class MainScene : SKScene, SKPhysicsContactDelegate {
     
     // SKPhysicsContactDelegate's methods
     func didBeginContact(contact: SKPhysicsContact) {
-    
+//        print("didBeginContact")
+        self .runAction(resetSound)
     }
     
     func didEndContact(contact: SKPhysicsContact) {
